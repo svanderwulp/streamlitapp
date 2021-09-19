4874a43fc99629c7b66e5cffb114fdd562bd79c9
Bas van der Wulp <bas.vanderwulp@fontys.nl>
2021-09-19 08:50:25 +0000

    fixed start_stream script

--- start_streamlit.sh
+++ start_streamlit.sh
@@ -3,7 +3,9 @@ LOGFILE="/var/log/streamlit.log"
 LOGLEVEL="debug"
 BASEURL=""
 
-if [-z "${BASEURL}"]; then
+source /opt/ownr/python/venv/bin/activate
+
+if [ -z "${BASEURL}"]; then
     (streamlit run app.py --server.enableXsrfProtection false --server.enableCORS false --logger.level=${LOGLEVEL}) > ${LOGFILE} 2>&1
 else
     (streamlit run app.py --server.baseUrlPath ${BASEURL} --logger.level=${LOGLEVEL}) > ${LOGFILE} 2>&1
