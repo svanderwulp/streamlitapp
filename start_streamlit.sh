2b461fd593a2e3530948fcf297b7f0f1eb61c207
Bas van der Wulp <bas.vanderwulp@fontys.nl>
2021-09-19 10:35:47 +0200

    reverted start_streamlit to version with cors disabled

--- start_streamlit.sh
+++ start_streamlit.sh
@@ -1,2 +1,10 @@
-source /opt/ownr/python/venv/bin/activate
-streamlit run app.py --server.enableXsrfProtection false --server.enableCORS false
+#!/bin/bash
+LOGFILE="/var/log/streamlit.log"
+LOGLEVEL="debug"
+BASEURL=""
+
+if [-z "${BASEURL}"]; then
+    (streamlit run app.py --server.enableXsrfProtection false --server.enableCORS false --logger.level=${LOGLEVEL}) > ${LOGFILE} 2>&1
+else
+    (streamlit run app.py --server.baseUrlPath ${BASEURL} --logger.level=${LOGLEVEL}) > ${LOGFILE} 2>&1
+fi
